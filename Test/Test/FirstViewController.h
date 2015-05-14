8cf92a3d73d4e2c8665a896c88a3686f7479b31f
wk <wk_51920@126.com>
2015-05-14 22:23:26 +0800

    second commit

---
+++ Test/Test/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Test
+//
+//  Created by wk on 15/5/14.
+//  Copyright (c) 2015年 WK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Test
+//
+//  Created by wk on 15/5/14.
+//  Copyright (c) 2015年 WK. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
